88fd4458f9dadaa4f21ed596a08d63d8cdc7cb7e
Edizon Basseto <edizon.basseto@outlook.com>
2019-11-23 10:01:51 -0300

    Increasing 3pp versions

--- wibx/contracts/VestingLib.sol
+++ wibx/contracts/VestingLib.sol
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, version 2.0: https://github.com/wibxcoin/Contracts/LICENSE.txt
  */
 
-pragma solidity 0.5.12;
+pragma solidity 0.5.0;
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./TaxLib.sol";
